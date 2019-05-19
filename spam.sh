dcb5d395e82cb5fc1e4db095d4b40b7b6e8bac8f
Mr Zhu <50517734+banghyuu@users.noreply.github.com>
2019-05-19 07:52:00 +0000

    Add files via upload

---
+++ spam.sh
@@ -0,0 +1,108 @@
+clear
+toilet -f mono12 -F metal "MR ZHU"
+sleep 1
+echo "\033[31;1m ===============TOOL SPAMMER================"
+echo "\033[31;1m My Team   : Indonesian  Hazyweb  Team"
+echo "\033[36;1m Author by : Mr Zhu"
+echo "\033[36;1m Kontak WA : 0859175727510"
+echo "\033[32;1m Info      : Sebelum memilih spam"
+echo "\033[32;1m           : Install bahan terlebih dahulu"
+echo "\033[32;1m	   : Pilih Angka [08]"
+echo "\033[38;1m Thanks To : Allah SWT | And All Member IHT "
+echo "\033[38;1m Special   : KUHAKU >> Is My Friend	    "
+echo "\033[31;1m =============== MR ZHU 404 ================"
+
+echo ""
+sleep 1
+     echo "\033[34;1m SPAM SMS Yang Tersedia "
+     echo "   '(01)'JDID "
+     echo "   '(02)'TELKOMSEL "
+     echo "   '(03)'PHD.      "
+     echo "   '(04)'MATHARIMALL"
+     echo "   '(05)'GRAB "
+     echo "   '(06)'TOKOPEDIA"
+sleep 1
+echo 
+     echo "    (07)INFO "
+     echo "    (08)Install Bahan"
+     echo "    (09)Keluar "
+echo " Pilih nomornya boss q "
+read ezz
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+echo "\033[31;1m"
+toilet -f mono12 -F gay "JDID"
+php jdid.php
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+echo "\033[36;1m"
+toilet -f  mono12 -F gay "TELKOMSEL"
+php telkomsel.php
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+echo  "\033[39;1m"
+toilet -f mono12 -F metal "PHD"
+php phd.php
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "MATAHARIMALL"
+php mataharimall.php
+fi
+
+if [$ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "GRAB"
+python2 spammer.py
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "TOKPED"
+php tokped.php
+fi
+
+if [ $ezz = 07 ] || [ $ezz = 7 ]
+then 
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "INFO"
+echo "Dibuat oleh "
+echo "Dikembangkan oleh ???"
+echo "Mr Zhu | Indonesian Hazyweb Team"
+echo "versi 0.1"
+echo "THANKS TO ALL MEMBER IHT"
+echo ""
+fi
+
+
+if [ $ezz = 08 ] || [ $ezz =8 ]
+then
+clear
+echo "Proses Install berjalan..."|lolcat
+sh install.sh
+sh gco.sh
+fi
+
+if [ $ezz = 09 ] || [ $ezz =9 ]
+then
+clear
+echo "\033[39;1m"
+toilet -f mono12 -F metal "KELUAR"
+echo "Terima kasih tamu Mr Zhu"
+fi
+
